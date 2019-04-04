3fd5511c4724b83816ab0fcfb04cf4ce7bcb53cd
shawnvdp <shawneevdploeg@live.nl>
2019-04-04 15:42:15 +0200

    Moved views files into a dedicated folder so we can keep RESTful convention for nested routes. Added 'NEW' and 'CREATE' comment routes, added a button to create a new comment. Fixed partials linking (../ to go up 1 level since we added new dedicated views folders).

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-let expressSanitizer    = require("express-sanitizer"),
-methodOverride          = require("method-override"),
-bodyParser              = require("body-parser"),
-mongoose                = require("mongoose"),
-express                 = require("express"),
-seedDB                  = require("./seeds"),
-Blog                    = require("./models/blog"),
-comments                = require("./models/comment"),
-app                     = express();
+let expressSanitizer = require("express-sanitizer"),
+	methodOverride = require("method-override"),
+	bodyParser = require("body-parser"),
+	mongoose = require("mongoose"),
+	express = require("express"),
+	app = express(),
+	seedDB = require("./seeds"),
+	Comment = require("./models/comment"),
+	Blog = require("./models/blog");
 
 //create local mongodb if it doesn't exist, otherwise use it
-mongoose.connect("mongodb://localhost/restful_blog", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost/restful_blog", {
+	useNewUrlParser: true
+});
 
 //express setup
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(
+	bodyParser.urlencoded({
+		extended: true
+	})
+);
 //must go after bodyParser
 app.use(expressSanitizer());
 //configure the string Method_Override looks for (put/delete requests)
@@ -23,101 +29,149 @@ app.use(methodOverride("_method"));
 seedDB();
 
 //ROOT
-app.get("/", function(req, res){
-    res.redirect("blogs");
+app.get("/", function(req, res) {
+	res.redirect("blogs");
 });
 
 //INDEX ROUTE
-app.get("/blogs", function(req, res){
-    //find all entries in our database, handle err/results in callback function
-    Blog.find({}, function(err, blogs){
-        if (err){
-            console.log("ERROR");
-        } else{
-            //pass blog results into index.ejs, calling it 'blogs'
-            res.render("index", {blogs: blogs});
-        }
-    });
+app.get("/blogs", function(req, res) {
+	//find all entries in our database, handle err/results in callback function
+	Blog.find({}, function(err, blogs) {
+		if (err) {
+			console.log("ERROR");
+		} else {
+			//pass blog results into index.ejs, calling it 'blogs'
+			res.render("blogs/index", {
+				blogs: blogs
+			});
+		}
+	});
 });
 
 //NEW ROUTE
-app.get("/blogs/new", function(req, res){
-    res.render("new");
+app.get("/blogs/new", function(req, res) {
+	res.render("/blogs/new");
 });
 
 //CREATE
-app.post("/blogs", function(req, res){
-    //grab the form data from the request's body
-    let formData = req.body.blog;
-    //req.body = form data, blog.body = 'name' attribute object, set that equal to sanitized version of current(strip script tags)
-    req.body.blog.body = req.sanitize(req.body.blog.body);
-    Blog.create(formData, function(err, newBlog){
-        if (err){
-            res.render("new");
-        } else{
-            res.redirect("/blogs");
-        }
-    });
+app.post("/blogs", function(req, res) {
+	//grab the form data from the request's body
+	let formData = req.body.blog;
+	//req.body = form data, blog.body = 'name' attribute object, set that equal to sanitized version of current(strip script tags)
+	req.body.blog.body = req.sanitize(req.body.blog.body);
+	Blog.create(formData, function(err, newBlog) {
+		if (err) {
+			res.render("blogs/new");
+		} else {
+			res.redirect("/blogs");
+		}
+	});
 });
 
 //SHOW
-app.get("/blogs/:id", function(req, res){
-    //grab the ':id' field from the url inside req.params
-    let blogID = req.params.id;
-    //populate the comments array holding Comment _id references, .exec to actually exec the query
-    Blog.findById(blogID).populate("comments").exec(function(err, foundBlog){
-        if (err){
-            res.redirect("index");
-            console.log(err);
-        } else{
-            res.render("show", {blog: foundBlog});
-        }
-    })
+app.get("/blogs/:id", function(req, res) {
+	//grab the ':id' field from the url inside req.params
+	let blogID = req.params.id;
+	//populate the comments array holding Comment _id references, .exec to actually exec the query
+	Blog.findById(blogID)
+		.populate("comments")
+		.exec(function(err, foundBlog) {
+			if (err) {
+				res.redirect("index");
+				console.log(err);
+			} else {
+				res.render("blogs/show", {
+					blog: foundBlog
+				});
+			}
+		});
 });
 
 //EDIT
-app.get("/blogs/:id/edit", function(req, res){
-    let blogID = req.params.id;
-    Blog.findById(blogID, function(err, foundBlog){
-        if (err){
-            res.redirect("/blogs");
-        } else{
-            res.render("edit", {blog: foundBlog});
-        }
-    });
+app.get("/blogs/:id/edit", function(req, res) {
+	let blogID = req.params.id;
+	Blog.findById(blogID, function(err, foundBlog) {
+		if (err) {
+			res.redirect("/blogs");
+		} else {
+			res.render("blogs/edit", {
+				blog: foundBlog
+			});
+		}
+	});
 });
 
 //UPDATE
-app.put("/blogs/:id", function(req, res){
-    let blogID = req.params.id;
-    //req.body = form data, blog.body = 'name' attribute object, set that equal to sanitized version of current(strip script tags)
-    req.body.blog.body = req.sanitize(req.body.blog.body);
-    //lookup matching entry and update in 1 method
-    Blog.findByIdAndUpdate(blogID, req.body.blog, function(err, updatedBlog){
-        if (err){
-            res.redirect("/blogs");
-        } else{
-            //redirect to blog's SHOW page
-            res.redirect("/blogs/" + blogID);
-        }
-    });
+app.put("/blogs/:id", function(req, res) {
+	let blogID = req.params.id;
+	//req.body = form data, blog.body = 'name' attribute object, set that equal to sanitized version of current(strip script tags)
+	req.body.blog.body = req.sanitize(req.body.blog.body);
+	//lookup matching entry and update in 1 method
+	Blog.findByIdAndUpdate(blogID, req.body.blog, function(err, updatedBlog) {
+		if (err) {
+			res.redirect("/blogs");
+		} else {
+			//redirect to blog's SHOW page
+			res.redirect("/blogs/" + blogID);
+		}
+	});
 });
 
 //DELETE
-app.delete("/blogs/:id", function(req, res){
-    let blogID = req.params.id;
-    //lookup matching entry and delete in 1 method
-    Blog.findByIdAndDelete(blogID, function(err){
-        if (err){
-            res.redirect("/blogs");
-        } else{
-            res.redirect("/blogs");
-        }
-    });
+app.delete("/blogs/:id", function(req, res) {
+	let blogID = req.params.id;
+	//lookup matching entry and delete in 1 method
+	Blog.findByIdAndDelete(blogID, function(err) {
+		if (err) {
+			res.redirect("/blogs");
+		} else {
+			res.redirect("/blogs");
+		}
+	});
 });
 
+/*
+ ** COMMENTS ROUTES **
+ */
+
+//NEW
+app.get("/blogs/:id/comments/new", function(req, res) {
+	let blogID = req.params.id;
+	Blog.findById(blogID, function(err, blog) {
+		if (err) {
+			console.log(err);
+			res.redirect("/blogs/" + blogID);
+		} else {
+			res.render("comments/new", { blog: blog });
+		}
+	});
+});
+
+//CREATE
+app.post("/blogs/:id/comments", function(req, res) {
+	let blogID = req.params.id;
+	Blog.findById(blogID, function(err, blog) {
+		if (err) {
+			console.log(err);
+			res.redirect("/blogs");
+		} else {
+			//pass in the comment object from the form's 'name' attributes (comment[author], comment[text])
+			//which is an exact match for our 'Comment' mongoose model {title: String, author: String}
+			Comment.create(req.body.comment, function(err, comment) {
+				if (err) {
+					console.log(err);
+				} else {
+					//push newly created comment into found blog's comments[]
+					blog.comments.push(comment);
+					blog.save();
+					res.redirect("/blogs/" + blog._id);
+				}
+			});
+		}
+	});
+});
 
 //serve server on port 3000
-app.listen(3000, function(){
-    console.log("RESTful blog now listening on port 3000.");
-})
+app.listen(3000, function() {
+	console.log("RESTful blog now listening on port 3000.");
+});
--- seeds.js
+++ seeds.js
@@ -1,36 +1,47 @@
 let mongoose = require("mongoose"),
-Comment      = require("./models/comment"),
-Blog         = require("./models/blog");
+    Comment = require("./models/comment"),
+    Blog = require("./models/blog");
 
 //placeholder blog data to create a few blogs on launch
-let data = [
-    {title: "1", image: "https://i.imgur.com/VLY6ZGt.jpg", body: "11"},
-    {title: "2", image: "https://i.imgur.com/Fo5gqZ4.jpg", body: "22"},
-    {title: "3", image: "https://i.imgur.com/MINW5Xv.jpg", body: "33"},
+let data = [{
+        title: "1",
+        image: "https://i.imgur.com/VLY6ZGt.jpg",
+        body: "11"
+    },
+    {
+        title: "2",
+        image: "https://i.imgur.com/Fo5gqZ4.jpg",
+        body: "22"
+    },
+    {
+        title: "3",
+        image: "https://i.imgur.com/MINW5Xv.jpg",
+        body: "33"
+    },
 ]
 
-function seedDB(){
+function seedDB() {
     //remove all blogs
-    Blog.remove({}, function(err){
-        if (err){
+    Blog.remove({}, function (err) {
+        if (err) {
             console.log(err);
-        } else{
+        } else {
             console.log("removed all blog posts");
             //once we have removed all blogs without errors, loop through the data array
-            data.forEach(function(post){
+            data.forEach(function (post) {
                 //create blog post for each entry in data array
-                Blog.create(post, function(err, postCreated){
-                    if (err){
+                Blog.create(post, function (err, postCreated) {
+                    if (err) {
                         console.log(err);
-                    } else{
+                    } else {
                         //create a placeholder comment
                         Comment.create({
                             text: "This is a comment",
                             author: "That one guy"
-                        }, function(err, comment){
-                            if (err){
+                        }, function (err, comment) {
+                            if (err) {
                                 console.log(err);
-                            } else{
+                            } else {
                                 //if we have successfully created a comment, add it to the newly created blog's 'comments' array
                                 postCreated.comments.push(comment);
                                 //actually save the new post including the comment to the DB
